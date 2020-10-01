a879cae5f79ca53a60afb210bc6f89f504e15f64
Mark Jansen <markwilemail@gmail.com>
2020-10-01 22:52:20 +0200

    Changed all Entities; no setter necessary for keyed objects (EF-miracles)

--- Architecture.DataAccess.CustomerEntities/Customer.cs
+++ Architecture.DataAccess.CustomerEntities/Customer.cs
@@ -4,8 +4,8 @@ namespace Architecture.DataAccess.CustomerEntities
 {
     public class Customer
     {
-        public int Id { get; private set; }
-        public Guid Guid { get; private set; }
+        public int Id { get; /*private set;*/ }
+        public Guid Guid { get; /*private set;*/ }
         public string FirstName { get; set; }
         public string Initials { get; set; }
         public string LastName { get; set; }
